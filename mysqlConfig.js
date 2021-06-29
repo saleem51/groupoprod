dec13695fa4933515fa1fe340a1a5806b17734e4
saleem51 <60423942+saleem51@users.noreply.github.com>
2021-06-29 10:31:01 +0200

    db_config

--- mysqlConfig.js
+++ mysqlConfig.js
@@ -3,17 +3,20 @@ const dotenv = require("dotenv");
 
 dotenv.config({path: './.env'}); //masquage des informations sensibles comme les idantifiants et les mots de passes
 
-const NAME = process.env.NAME;
-const PASS = process.env.PASS;
-const HOST = process.env.HOST;
+//const NAME = process.env.NAME;
+//const PASS = process.env.PASS;
+//const HOST = process.env.HOST;
 
+const HOST = mysql-saleem51.alwaysdata.net 
+const NAME = saleem51@2a00:b6e0:1:210:1::1
+const PASS = Tutrouverapa26?
 //Connexion à la base de donnée Mysql
 
 const db = mysql.createConnection({
     host     : HOST,
     user     : NAME,
     password : PASS,
-    database: 'Groupomania'
+    database: 'saleem51_groupomania'
 });
 
 db.connect((err) => {
